5d419cab9e7bed32bcfb67c03ca59f2ab74d8edd
Vendula Krajíčková <vendysacek@seznam.cz>
2024-05-16 13:12:33 +0200

    #1 - create RuleItem comp

--- src/components/Rules/Rules.tsx
+++ src/components/Rules/Rules.tsx
@@ -1,6 +1,5 @@
+import { RuleItem } from '../RuleItem/RuleItem';
 import './Rules.scss';
-import { FaCheckCircle } from "react-icons/fa";
-import { FaRegCircle } from "react-icons/fa";
 
 interface RulesProps {
   isInvalidPasswordCharacter: boolean;
@@ -16,33 +15,18 @@ export const Rules: React.FC<RulesProps> = ({
 
   return (
     <ul className="rules">
-      <li className={`item ${isInvalidPasswordCharacter ? '' : 'check'}`}>
-        {
-          isInvalidPasswordCharacter ?
-            <FaRegCircle className="icon-circle" />
-            :
-            <FaCheckCircle className="icon-circle check" />
-        }
-        8 characters
-      </li>
-      <li className={`item ${isInvalidPasswordLetter ? '' : 'check'}`}>
-        {
-          isInvalidPasswordLetter ?
-            <FaRegCircle className="icon-circle" />
-            :
-            <FaCheckCircle className="icon-circle check" />
-        }
-        1 uppercase letter
-      </li>
-      <li className={`item ${isInvalidPasswordNumber ? '' : 'check'}`}>
-        {
-          isInvalidPasswordNumber ?
-            <FaRegCircle className="icon-circle" />
-            :
-            <FaCheckCircle className="icon-circle check" />
-        }
-        1 number
-      </li>
+      <RuleItem
+        invalidRule={isInvalidPasswordCharacter}
+        name="8 characters"
+      />
+      <RuleItem
+        invalidRule={isInvalidPasswordLetter}
+        name="1 uppercase letter"
+      />
+      <RuleItem
+        invalidRule={isInvalidPasswordNumber}
+        name="1 number"
+      />
     </ul>
   )
 }
