100693260f435c5eebffafe2edfa8cde36a5f711
Vendula Krajíčková <vendysacek@seznam.cz>
2024-05-29 14:23:03 +0200

    edit getDate use adjusted

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,8 +1,14 @@
 export const formatDate = (date: Date): string => {
-  const day = date.getDate().toString().padStart(2, '0');
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const year = date.getFullYear();
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const adjustedDate = new Date(date.getTime());
+
+  adjustedDate.setHours(adjustedDate.getHours() - 5);
+  adjustedDate.setMinutes(adjustedDate.getMinutes() - 23);
+
+  const day = adjustedDate.getDate().toString().padStart(2, '0');
+  const month = (adjustedDate.getMonth() + 1).toString().padStart(2, '0');
+  const year = adjustedDate.getFullYear();
+  const hours = adjustedDate.getHours().toString().padStart(2, '0');
+  const minutes = adjustedDate.getMinutes().toString().padStart(2, '0');
+
   return `${day}.${month}.${year} ${hours}:${minutes}`;
 }
